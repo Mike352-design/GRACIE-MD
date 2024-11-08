1e1895dda65ec1c47b5d8414a660716b69c76f10
Mike352-design <patrickamau702@gmail.com>
2024-11-08 08:27:30 +0300

    Update popkidcrick.js

--- scs/bmworder/popkidcrick.js
+++ scs/bmworder/popkidcrick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *POPKID MD*\n`;
+      formattedResult += `│⿻   *99 CLAN MD*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
