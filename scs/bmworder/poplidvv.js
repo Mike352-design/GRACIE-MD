c877242daee245b969f651decd57e19616b42419
Mike352-design <patrickamau702@gmail.com>
2024-11-08 08:42:19 +0300

    Update poplidvv.js

--- scs/bmworder/poplidvv.js
+++ scs/bmworder/poplidvv.js
@@ -23,7 +23,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const type = Object.keys(msg)[0];
     
     const originalCaption = msg[type].caption || '';
-    const newCaption = `${originalCaption}\n\n> © Powered By POPKID-MD`;
+    const newCaption = `${originalCaption}\n\n> © Powered By Alvino==>view once n scum😂`;
 
 
     // Download the media content
