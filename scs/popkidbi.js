14830386eb1265456c4ae8aaed9e09e1f3e0877c
Mike352-design <patrickamau702@gmail.com>
2024-11-08 07:49:20 +0300

    Update popkidbi.js

--- scs/popkidbi.js
+++ scs/popkidbi.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { adams } = require("../Ibrahim/adams");
 adams({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🌍𝗣𝗢𝗣𝗞𝗜𝗗 𝗜𝗦 𝗢𝗡𝗟𝗜𝗡𝗘🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
+    let z = '*🌍99 CLAN MD 𝗜𝗦 𝗢𝗡𝗟𝗜𝗡𝗘🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
